089027013e9338877ef3448734e1ba4d997f9c81
fujimon0211 <89776152+fujimon0211@users.noreply.github.com>
2021-10-23 17:19:29 +0900

    d

---
+++ test.js
@@ -0,0 +1,53 @@
+$(function(){
+
+  $('#one').fadeIn();
+  $(".two").fadeIn();
+  $(".third").fadeIn();
+  $(".four").fadeIn();
+  $(".five").fadeIn();
+  $(".six").fadeIn();
+  $(".seven").fadeIn();
+  $(".eight").fadeIn();
+  $(".nine").fadeIn();
+  $(".ten").fadeIn();
+ 
+
+});
+function EachTextAnimeControl() {
+    $('.eachTextAnime').each(function () {
+      var elemPos = $(this).offset().top - 50;
+      var scroll = $(window).scrollTop();
+      var windowHeight = $(window).height();
+      if (scroll >= elemPos - windowHeight) {
+        $(this).addClass("appeartext");
+  
+      } else {
+        $(this).removeClass("appeartext");
+      }
+    });
+  }// 画面が読み込まれたらすぐに動かしたい場合の記述
+$(window).on('load', function () {
+    //spanタグを追加する
+    var element = $(".eachTextAnime");
+    element.each(function () {
+      var text = $(this).text();
+      var textbox = "";
+      text.split('').forEach(function (t, i) {
+        if (t !== " ") {
+          if (i < 10) {
+            textbox += '<span style="animation-delay:.' + i + 's;">' + t + '</span>';
+          } else {
+            var n = i / 10;
+            textbox += '<span style="animation-delay:' + n + 's;">' + t + '</span>';
+          }
+  
+        } else {
+          textbox += t;
+        }
+      });
+      $(this).html(textbox);
+    });
+  
+    EachTextAnimeControl();/* アニメーション用の関数を呼ぶ*/
+  });// ここまで画面が読み込まれたらすぐに動かしたい場合の記述
+
